ae54d0b184af5847850cdf97ab24050cd0b048ea
ersanbr <ers2003@gmail.com>
2017-04-20 15:46:51 -0300

    exclusão dos arquivos do projeto antigo

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$conn=new PDO('mysql:host=localhost;dbname=monsters','monsters','university');
-	$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    /*** echo a message saying we have connected ***/
-    
-?>
